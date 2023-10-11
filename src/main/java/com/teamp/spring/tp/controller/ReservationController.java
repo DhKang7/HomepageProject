145d833293707d6d17936fca7c8e26c4e7fc71a9
namjunu <hsson040@gmail.com>
2023-10-11 09:58:49 +0900

    commit

--- src/main/java/com/teamp/spring/tp/controller/ReservationController.java
+++ src/main/java/com/teamp/spring/tp/controller/ReservationController.java
@@ -1,9 +1,7 @@
 package com.teamp.spring.tp.controller;
 
 import com.teamp.spring.tp.dto.ReservationDto;
-
 import com.teamp.spring.tp.service.ReservationService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,15 +10,13 @@ import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
-@Controller
-@RequestMapping("/reservation/*")
-public class ReservationController {
-    private final ReservationService reservationService;
 
-    @Autowired
-    public ReservationController(ReservationService reservationService) {
-        this.reservationService = reservationService;
-    }
+@Log4j
+@AllArgsConstructor
+@RequestMapping("/reservation/*")
+@Controller
+public class ReservationController {
+    private ReservationService reservationService;
 
     @GetMapping("/list")
     public ModelAndView booklist(@RequestParam String R_name) {
@@ -47,4 +43,5 @@ public class ReservationController {
         reservationService.bookmodify(reservationDto);
         return "redirect:/reservation/list?R_name=" + reservationDto.getR_name();
     }
+
 }
