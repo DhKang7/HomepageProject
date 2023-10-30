2938cfc95bf23990ed0f97a76a9ba870d0a6cd0f
wsbh <bboy9903@gmail.com>
2023-10-30 09:08:25 +0900

    ver1.1.1
    1. JFrame이 옛날 java기술이어서 그런지 @Service -> Mapper로 가는 행위가 호환이 되지 않음.
    역순으로 해도 같은 현상 발생
    그래서 @Service-> Mapper로 가는 것을 제거하고 클래스 바로 연동되도록 변경

--- src/main/java/com/teamp/spring/JangController.java
+++ src/main/java/com/teamp/spring/JangController.java
@@ -3,10 +3,7 @@ package com.teamp.spring;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.teamp.jang.kiosk.KioskIns1;
-import com.teamp.jang.kiosk.KioskIns4;
 import com.teamp.jang.kiosk.KioskInsFinal;
-import com.teamp.jang.kiosk.KioskScreen;
 
 import lombok.AllArgsConstructor;
 
@@ -14,12 +11,12 @@ import lombok.AllArgsConstructor;
 @Controller
 @RequestMapping("jang")
 public class JangController {
-
+	
+	
 	@RequestMapping("/Kiosk")
 	public void Kisok() {
-		KioskInsFinal kioskScreen = new KioskInsFinal();
-		kioskScreen.KioskGUI();
-		
+		KioskInsFinal kioskplay = new KioskInsFinal();
+		kioskplay.KioskGUI();
 	}
 	
 }
